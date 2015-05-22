e615abcd2cc22b7e2cdcfdf7b47f1aab4885b907
plmn1988 <threesixster@gmail.com>
2015-05-22 21:53:33 +0300

    Created project base structure

---
+++ js/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var app = angular.module('SocialNetwork', ['ngRoute', 'ngResource', 'ui.bootstrap.pagination']);
+
+app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net');
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: 'templates/partials/user-login-register.html',
+        controller: ''
+    });
+
+    $routeProvider.when('/users/:username', {
+        templateUrl: 'templates/user-wall.html',
+        controller: ''
+    });
+
+    $routeProvider.when('/users/:username/friends', {
+        templateUrl: 'templates/friends-sidebar.html',
+        controller: ''
+    });
+
+    $routeProvider.when('/profile', {
+        templateUrl: 'templates/profile-edit.html',
+        controller: ''
+    });
+
+    $routeProvider.when('/logout', {
+        templateUrl: 'templates/logout.html',
+        controller: ''
+    });
+
+    $routeProvider.otherwise(
+        { redirectTo: '/' }
+    );
+
+});
